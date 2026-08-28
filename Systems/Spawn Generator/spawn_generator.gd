c0d03f695d8e014cee422be68a7927a74db9fa84
Crimtane <said-h95@hotmail.com>
2026-08-28 17:25:13 +0200

    Fixed an issue with pickup spawner
    Made it so that pickup items can only spawn on tiles that has type "floor" string attached to them.

--- Systems/Spawn Generator/spawn_generator.gd
+++ Systems/Spawn Generator/spawn_generator.gd
@@ -5,9 +5,11 @@ var pickupScene = preload("uid://b1v2i1jgbgqj2")
 #var pickupSpawnTimer: int = 10 
 
 var player
+var tilemap: TileMapLayer
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("Player")
+	tilemap = get_tree().current_scene.get_node("Dungeon Map/BackgroundLayer")
 	$"Buff Spawn Timer".timeout.connect(onSpawnBuffTimerTimeout)
 	
 
@@ -33,9 +35,19 @@ func createBuffPickup() -> void:
 	var pickupTypes = ["Health", "Damage", "Shield", "Speed"]
 	pickup.pickupSprite = pickupTypes.pick_random()
 	
-	var randomOffset = Vector2.from_angle(randf_range(0, TAU)) * randf_range(100, 200) # circle
+	var keepLooping = true
+	var randomPosition: Vector2
 	
-	pickup.global_position = player.global_position + randomOffset
+	while keepLooping:
+		var randomOffset = Vector2.from_angle(randf_range(0, TAU)) * randf_range(100, 200) # circle
+		var localRandomPosition = player.position + randomOffset
+		var cellGridPosition = tilemap.local_to_map(localRandomPosition)
+		var cell = tilemap.get_cell_tile_data(cellGridPosition)
+		if cell and cell.get_custom_data("type") == "floor":
+			randomPosition = player.global_position + randomOffset
+			keepLooping = false
+	
+	pickup.global_position = randomPosition
 	
 	get_parent().call_deferred("add_child", pickup)
 
